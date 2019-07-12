761e731b387983fc942f98ef2440a81ef92a67ee
Jenna Cavrich <jenna.cavrich@gmail.com>
2019-07-12 17:30:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case (age<=19 && age>=13) :
+    case (15) :
     return "You are a teenager"
     break;
     default:
